e56f80550063a5ee6d2f19e0a9bb9edbc9306ff3
Artem Kosharnyi <artemkkosharnyi@gmali.com>
2023-04-01 13:44:42 +0300

    add interval to update currency prise

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,8 @@ function Header() {
   const [usdToUahRate, setUsdToUahRate] = useState("");
   const [eurToUahRate, setEurToUahRate] = useState("");
  // API functions
-  useEffect(() => {
+ useEffect(() => {
+  const interval = setInterval(() => {
     fetch("https://api.exchangerate-api.com/v4/latest/USD")
       .then((response) => response.json())
       .then((data) => {
@@ -16,8 +17,11 @@ function Header() {
       .then((data) => {
         setEurToUahRate(data.rates.UAH.toFixed(2));
       });
-  }, []);
+  }, 60000); // update every 1 hour
 
+  // cleanup interval on unmount
+  return () => clearInterval(interval);
+}, []);
   return (
     <div className="flex justify-center gap-20 p-2 bg-[#1B1B1B] text-white absolute top-0 right-0 w-screen z-10">
       <div>1 USD = {usdToUahRate} UAH</div>
